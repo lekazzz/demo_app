6040dab230e7b5f3b23e0d54eaf59427724d2a8f
Lekaz <lekazzz@outlook.com>
2016-04-14 22:02:55 +0300

    install cmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '1b120d6a3bea54ad0bd6310eaec0519998f7b3ce82d2a41463ad70582b44bab2756e7172ead8b387a389598e73254ba4a0ca42b87f35d3a76ba79a1248cdca01'
